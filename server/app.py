4ab2742c533d8a590e4e1ed8907b36355144a0e0
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-07-10 09:54:26 +0300

    adds destination class and the get, post, delete and patch functinos

--- server/app.py
+++ server/app.py
@@ -60,6 +60,39 @@ class UserByID(Resource):
             return make_response(user.to_dict(), 200)
         
         return make_response({'error': 'User not found'}, 404)
+    
+class Destinations(Resource):
+    
+    def get(self):
+        destinations = [destination.to_dict() for destination in Destination.query.all()]
+        return make_response(destinations, 200)
+    
+    def post(self):
+        data = request.json
+        destination = Destination(name=data['name'], location=data['location'], description=data['description'], image=data['image'])
+        db.session.add(destination)
+        db.session.commit()
+        return make_response(destination.to_dict(), 201)
+    
+    def delete(self, id):
+        destination = Destination.query.filter_by(id= id).first()
+        if destination:
+            db.session.delete(destination)
+            db.session.commit()
+            return make_response(destination.to_dict(), 200)
+        else:
+            return make_response({'error': 'Destination not found'}, 404)
+        
+    def patch(self, id):
+        destination = Destination.query.filter_by(id=id).first()
+        for attr in request.json:
+            setattr(destination, attr, request.json[attr])
+            
+        db.session.add(destination)
+        db.session.commit()
+        return make_response(destination.to_dict(), 200)
+    
+
 
 
 if __name__ == '__main__':
