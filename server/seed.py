8634be2185f990d8fd8026b57be5e8ffaafac137
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-07-10 09:58:32 +0300

    adds the make_users seed code

--- server/seed.py
+++ server/seed.py
@@ -5,13 +5,30 @@ from random import randint, choice as rc
 
 # Remote library imports
 from faker import Faker
+fake = Faker()
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Destination, Review
+
+def make_users():
+    print("Seeding users...") 
+    User.query.delete()
+    
+    users = []
+    
+    username = [fake.unique.first_name() for _ in range(20)]
+    email = [fake.email() for _ in range(20)]
+    password = [fake.password(length=8) for _ in range(20)]
+    
+    for i in range(20):
+        user = User(username=username[i], email=email[i], password=password[i])
+        users.append(user)
+    
+    db.session.add_all(users)
+    db.session.commit()
 
 if __name__ == '__main__':
-    fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
