c70b27c0c2488fafb4b28c05c75b64e5efcb8d3a
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-07-10 09:51:46 +0300

    adds the user class and the get, post, delete and patch functions

--- server/app.py
+++ server/app.py
@@ -3,12 +3,14 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import db, User, Destination, Review
+
 
 
 # Views go here!
@@ -16,6 +18,38 @@ from config import app, db, api
 @app.route('/')
 def index():
     return '<h1>Wanderers</h1>'
+
+class Users(Resource):
+    
+    def get(self):
+        users = [user.to_dict() for user in User.query.all()]
+        return make_response(users, 200)
+    
+    def post(self):
+        data = request.get_json()
+        user = User(username=data['username'], email=data['email'], password=data['password'])
+        db.session.add(user)
+        db.session.commit()
+        return make_response(user.to_dict(), 201)
+    
+    def delete(self, id):
+        user = User.query.filter_by(id= id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response({'error': 'User not found'}, 404)
+        
+    def patch(id):
+        user = User.query.filter_by(id= id).first()
+        if user:
+            for attr in request.json:
+                setattr(user, attr, request.json[attr])
+            db.session.add(user)
+            db.session.commit()
+        else:
+            return make_response({'error': 'User not found'}, 404)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
+import re 
 
 from config import db
 
