d92af73696004a729358a9dd4e45c60e65a6083c
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-07-14 22:25:45 +0300

    fixes few minor issues

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,8 +8,8 @@ function NavBar() {
         className="navbar navbar-expand-lg sticky-top"
         style={{ backgroundColor: "#252932" }}
       >
-        <div class="container">
-          <a class="navbar-brand px-4 text-white" href="/">
+        <div className="container">
+          <a className="navbar-brand px-4 text-white" href="/">
             <img
               className="image-fluid me-2 "
               src={backpackIcon}
@@ -20,7 +20,7 @@ function NavBar() {
             Wanderers
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -28,33 +28,36 @@ function NavBar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <a
-                  class="nav-link active  text-white"
+                  className="nav-link active  text-white"
                   aria-current="page"
                   href="/"
                 >
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="/travelguides">
+              <li className="nav-item">
+                <a className="nav-link text-white" href="/travelguides">
                   Reviews
                 </a>
               </li>
             </ul>
-            <form class="d-flex" role="search">
+            <form className="d-flex" role="search">
               <input
-                class="form-control me-2"
+                className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button class="btn btn-outline-success text-white" type="submit">
+              <button
+                className="btn btn-outline-success text-white"
+                type="submit"
+              >
                 Search
               </button>
             </form>
