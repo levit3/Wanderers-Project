605a9d1531a9d7251492bf282e4d6b52d09dff68
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-07-16 13:00:06 +0300

    makes minor changes

--- server/config.py
+++ server/config.py
@@ -8,9 +8,6 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_bcrypt import Bcrypt
-from flask_session import Session
-
-
 
 # Local imports
 
@@ -20,7 +17,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SESSION_COOKIE_SAMESITE"] = "None"
 app.config["SESSION_COOKIE_SECURE"] = True
-
 
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
@@ -40,6 +36,4 @@ api = Api(app)
 CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}},)
 
 
-SESSION_TYPE = 'filesystem'
-app.config.from_object(__name__)
-Session(app)
+
